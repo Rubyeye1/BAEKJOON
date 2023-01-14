2af94d6716e46433e597aa11dd6ff79908ab413d
박형욱 <118232537+Rubyeye1@users.noreply.github.com>
2023-01-14 12:24:15 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 0;
+
+	scanf("%d", &a);
+
+	if ((a % 400) == 0)
+	{
+		printf("1");
+	}
+	else if ((a % 4 == 0) && (a % 100 != 0))
+	{
+		printf("1");
+	}
+	else
+	{
+		printf("0");
+	}
+
+	return 0;
+}
