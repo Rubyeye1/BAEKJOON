2b9f0a2082af968e5e8125cd110913919f5104a2
박형욱 <118232537+Rubyeye1@users.noreply.github.com>
2023-02-22 11:05:41 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+#include <math.h>
+
+
+int main() {
+    long long A;
+    long long B;
+    long long C;
+
+    scanf("%ld %ld %ld", &A, &B, &C);
+
+    printf("%ld", A + B + C);
+
+
+
+
+    return 0;
+}
