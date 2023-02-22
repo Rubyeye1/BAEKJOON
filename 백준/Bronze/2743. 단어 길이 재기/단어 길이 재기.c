fa5be50d0defe3133d14f8723b916c2b49723da2
박형욱 <118232537+Rubyeye1@users.noreply.github.com>
2023-02-22 12:33:32 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+#include <math.h>
+
+
+int main() {
+
+    char a[101];
+    scanf("%s", &a);
+    int len;
+    len = strlen(a);
+    printf("%d", len);
+
+    return 0;
+}
