17ae1568d32307f771a00226d2442021010ae714
박형욱 <118232537+Rubyeye1@users.noreply.github.com>
2023-02-22 11:51:31 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+#include <math.h>
+
+
+int main() {
+
+    int N;
+    scanf("%d", &N);
+
+    int i;
+    int temp = N/4;
+    for (i = 1; i < temp; i++)
+    {
+        printf("long ");
+    }
+
+    printf("long int");
+
+   
+
+    return 0;
+}
