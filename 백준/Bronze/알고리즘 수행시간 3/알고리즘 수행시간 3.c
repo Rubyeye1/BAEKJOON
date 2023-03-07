181e38d17f064a7b4129af503f9499efa9697838
박형욱 <118232537+Rubyeye1@users.noreply.github.com>
2023-03-07 19:54:46 +0900

    Add files via upload

---
+++ 백준/Bronze/알고리즘 수행시간 3/알고리즘 수행시간 3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+#include <math.h>
+
+int main() {
+   
+    int n;
+    scanf("%d", &n);
+    int N = n * n;
+    printf("%d\n2",N);
+    
+    return 0;
+}
+
+
