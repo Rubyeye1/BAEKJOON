dc5fdee2dfb9f884df9cb250b07c3602fe7ead60
박형욱 <118232537+Rubyeye1@users.noreply.github.com>
2023-03-09 09:45:33 +0900

    Add files via upload

---
+++ 백준/Bronze/문자와 문자열/문자와 문자열.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <limits.h>
+#include <math.h>
+#include <string.h>
+
+int main(void)
+{
+	char a[1001];
+	int b = 0;
+	scanf("%s", &a);
+	scanf("%d", &b);
+	
+	printf("%c", a[b-1]);
+
+
+
+
+
+	return 0;
+}	
