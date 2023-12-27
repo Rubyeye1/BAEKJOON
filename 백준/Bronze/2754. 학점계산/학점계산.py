64f549db2d263c22d74b80962253d7a4f0898a37
박형욱 <118232537+Rubyeye1@users.noreply.github.com>
2023-12-27 10:12:26 +0900

    [Bronze V] Title: 학점계산, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.py
@@ -0,0 +1,28 @@
+hakjum = input()
+
+if hakjum == "A+":
+    print("4.3")
+elif hakjum == "A0":
+    print("4.0")
+elif hakjum == "A-":
+    print("3.7")
+elif hakjum == "B+":
+    print("3.3")
+elif hakjum == "B0":
+    print("3.0")
+elif hakjum == "B-":
+    print("2.7")
+elif hakjum == "C+":
+    print("2.3")
+elif hakjum == "C0":
+    print("2.0")
+elif hakjum == "C-":
+    print("1.7")
+elif hakjum == "D+":
+    print("1.3")
+elif hakjum == "D0":
+    print("1.0")
+elif hakjum == "D-":
+    print("0.7")
+else:
+    print("0.0")
