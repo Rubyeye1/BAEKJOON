3f7c8b7e6927aad12828cf5c17474eddc17d6363
박형욱 <118232537+Rubyeye1@users.noreply.github.com>
2024-03-04 14:44:25 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 92 ms, Memory: 36280 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.py
@@ -0,0 +1,90 @@
+import math
+import sys
+from collections import deque
+import copy
+
+S = sys.stdin.readline()
+S = list(S)
+S.pop()
+
+a=0
+b=0
+c=0
+d=0
+e=0
+f=0
+g=0
+h=0
+I=0
+j=0
+k=0
+l=0
+m=0
+n=0
+o=0
+p=0
+q=0
+r=0
+s=0
+t=0
+u=0
+v=0
+w=0
+x=0
+y=0
+z=0
+for i in range(len(S)):
+    if S[i] == 'a':
+        a += 1
+    elif S[i] == 'b':
+        b += 1
+    elif S[i] == 'c':
+        c += 1
+    elif S[i] == 'd':
+        d += 1
+    elif S[i] == 'e':
+        e += 1
+    elif S[i] == 'f':
+        f += 1
+    elif S[i] == 'g':
+        g += 1
+    elif S[i] == 'h':
+        h += 1
+    elif S[i] == 'i':
+        I += 1
+    elif S[i] == 'j':
+        j += 1
+    elif S[i] == 'k':
+        k += 1
+    elif S[i] == 'l':
+        l += 1
+    elif S[i] == 'm':
+        m += 1
+    elif S[i] == 'n':
+        n += 1
+    elif S[i] == 'o':
+        o += 1
+    elif S[i] == 'p':
+        p += 1
+    elif S[i] == 'q':
+        q += 1
+    elif S[i] == 'r':
+        r += 1
+    elif S[i] == 's':
+        s += 1
+    elif S[i] == 't':
+        t += 1
+    elif S[i] == 'u':
+        u += 1
+    elif S[i] == 'v':
+        v += 1
+    elif S[i] == 'w':
+        w += 1
+    elif S[i] == 'x':
+        x += 1
+    elif S[i] == 'y':
+        y += 1
+    elif S[i] == 'z':
+        z += 1
+print(a, b, c, d, e, f, g, h, I, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z)
+
