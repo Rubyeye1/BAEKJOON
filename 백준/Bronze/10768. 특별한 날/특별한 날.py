2a0454aec9fd5e789994362fe031d5c9f3f749af
박형욱 <118232537+Rubyeye1@users.noreply.github.com>
2024-03-20 09:59:28 +0900

    [Bronze IV] Title: 특별한 날, Time: 80 ms, Memory: 36124 KB -BaekjoonHub

---
+++ 백준/Bronze/10768. 특별한 날/특별한 날.py
@@ -0,0 +1,19 @@
+import math
+import sys
+from collections import deque
+import copy
+
+A = int(input())
+B = int(input())
+
+if A < 2:
+    print("Before")
+elif A == 2:
+    if B < 18:
+        print("Before")
+    elif B == 18:
+        print("Special")
+    else:
+        print("After")
+else:
+    print("After")
