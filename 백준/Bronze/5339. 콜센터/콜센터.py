8d151ff78bb0b4a51a260a343ecfda4b35555908
박형욱 <118232537+Rubyeye1@users.noreply.github.com>
2024-03-29 12:46:51 +0900

    [Bronze V] Title: 콜센터, Time: 80 ms, Memory: 36132 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.py
@@ -0,0 +1,15 @@
+import math
+import sys
+from collections import deque
+import copy
+
+print("     /~\\")
+print("    ( oo|")
+print("    _\=/_")
+print("   /  _  \\")
+print("  //|/.\\|\\\\")
+print(" ||  \ /  ||")
+print("============")
+print("|          |")
+print("|          |")
+print("|          |")
