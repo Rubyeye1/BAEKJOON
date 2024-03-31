797b9ccb03cdf17c8e63c39515917c5c8160b104
박형욱 <118232537+Rubyeye1@users.noreply.github.com>
2024-04-01 08:38:33 +0900

    [Bronze V] Title: 오늘의 날짜는?, Time: 172 ms, Memory: 118732 KB -BaekjoonHub

---
+++ 백준/Bronze/16170. 오늘의 날짜는？/오늘의 날짜는？.py
@@ -0,0 +1,10 @@
+import math
+import sys
+from collections import deque
+import copy
+import datetime
+
+datetime = datetime.datetime.now()
+print(datetime.year)
+print(datetime.month)
+print(datetime.day)
