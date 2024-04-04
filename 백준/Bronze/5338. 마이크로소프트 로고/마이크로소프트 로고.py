5e6fa20750855773c69e59338b003d2001267e3c
박형욱 <118232537+Rubyeye1@users.noreply.github.com>
2024-04-04 18:51:34 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 92 ms, Memory: 38756 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.py
@@ -0,0 +1,11 @@
+import math
+import sys
+from collections import deque
+import copy
+import datetime
+
+print("       _.-;;-._")
+print("'-..-'|   ||   |")
+print("'-..-'|_.-;;-._|")
+print("'-..-'|   ||   |")
+print("'-..-'|_.-''-._|")
