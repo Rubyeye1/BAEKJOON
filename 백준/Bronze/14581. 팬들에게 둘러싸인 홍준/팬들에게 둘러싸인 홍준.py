d0e54d37f9f117162d012fe487c49b39f849dda7
박형욱 <118232537+Rubyeye1@users.noreply.github.com>
2024-04-07 11:55:28 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 96 ms, Memory: 38756 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.py
@@ -0,0 +1,10 @@
+import math
+import sys
+from collections import deque
+import copy
+import datetime
+
+hongjoon = str(input())
+print(":fan::fan::fan:")
+print(":fan::" + hongjoon + "::fan:")
+print(":fan::fan::fan:")
