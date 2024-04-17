ac386d6516cae8127222c62ff4838e7c509da9eb
박형욱 <118232537+Rubyeye1@users.noreply.github.com>
2024-04-18 00:17:16 +0900

    [Bronze III] Title: 네 수, Time: 112 ms, Memory: 38888 KB -BaekjoonHub

---
+++ 백준/Bronze/10824. 네 수/네 수.py
@@ -0,0 +1,11 @@
+import math
+import sys
+from collections import deque
+import copy
+import datetime
+
+
+a, b, c, d = map(str, input().split())
+resultA = a + b
+resultB = c + d
+print(int(resultA)+int(resultB))
