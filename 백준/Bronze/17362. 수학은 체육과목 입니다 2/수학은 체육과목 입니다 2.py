bfbbb3a31efb98652bd33fd138d80a6d53fd20b9
박형욱 <118232537+Rubyeye1@users.noreply.github.com>
2024-07-12 12:31:57 +0900

    [Bronze IV] Title: 수학은 체육과목 입니다 2, Time: 96 ms, Memory: 38764 KB -BaekjoonHub

---
+++ 백준/Bronze/17362. 수학은 체육과목 입니다 2/수학은 체육과목 입니다 2.py
@@ -0,0 +1,19 @@
+import math
+import sys
+from collections import deque
+import copy
+import datetime
+
+n = int(sys.stdin.readline())
+n = n % 8
+
+if n == 1:
+    print(1)
+elif n == 0 or n == 2:
+    print(2)
+elif n == 3 or n == 7:
+    print(3)
+elif n == 4 or n == 6:
+    print(4)
+else:
+    print(5)
