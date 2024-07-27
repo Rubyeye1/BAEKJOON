7a0381fdfb938d8564d3c7c41f6869f78e5bcf1e
박형욱 <118232537+Rubyeye1@users.noreply.github.com>
2024-07-28 06:30:53 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 88 ms, Memory: 38756 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.py
@@ -0,0 +1,11 @@
+import math
+import sys
+from collections import deque
+import copy
+import datetime
+
+N, K = map(int, sys.stdin.readline().split())
+for i in range(N):
+    a, b = map(int, sys.stdin.readline().split())
+    
+print("비와이")
