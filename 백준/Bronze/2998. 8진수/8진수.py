ca1915273e91c9f374cd2185fa599451868a553a
박형욱 <118232537+Rubyeye1@users.noreply.github.com>
2024-08-11 10:58:17 +0900

    [Bronze II] Title: 8진수, Time: 84 ms, Memory: 38760 KB -BaekjoonHub

---
+++ 백준/Bronze/2998. 8진수/8진수.py
@@ -0,0 +1,11 @@
+import math
+import sys
+from collections import deque
+import copy
+import datetime
+from datetime import date, datetime
+
+
+T = int(sys.stdin.readline(), 2)
+T = oct(T)
+print(T[2:])
