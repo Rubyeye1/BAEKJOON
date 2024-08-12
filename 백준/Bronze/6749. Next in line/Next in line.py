610085c9c6d2d69afd1229d400b47b8839786131
박형욱 <118232537+Rubyeye1@users.noreply.github.com>
2024-08-12 10:30:59 +0900

    [Bronze IV] Title: Next in line, Time: 96 ms, Memory: 38736 KB -BaekjoonHub

---
+++ 백준/Bronze/6749. Next in line/Next in line.py
@@ -0,0 +1,12 @@
+import math
+import sys
+from collections import deque
+import copy
+import datetime
+from datetime import date, datetime
+
+
+A = int(sys.stdin.readline())
+B = int(sys.stdin.readline())
+C = B + (B - A)
+print(C)
