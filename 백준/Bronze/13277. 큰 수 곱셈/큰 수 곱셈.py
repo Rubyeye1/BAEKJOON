fa8795957f37f7add654d8db48b1de807c3d25ec
박형욱 <118232537+Rubyeye1@users.noreply.github.com>
2024-08-22 10:06:42 +0900

    [Bronze V] Title: 큰 수 곱셈, Time: 4712 ms, Memory: 39304 KB -BaekjoonHub

---
+++ 백준/Bronze/13277. 큰 수 곱셈/큰 수 곱셈.py
@@ -0,0 +1,10 @@
+import math
+import sys
+from collections import deque
+import copy
+import datetime
+from datetime import date, datetime
+
+
+a, b = map(int,sys.stdin.readline().split())
+print(a*b)
