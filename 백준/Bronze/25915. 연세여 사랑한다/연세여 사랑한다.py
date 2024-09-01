b2f2c363daf4315111597227baf5bf09a14173ed
박형욱 <118232537+Rubyeye1@users.noreply.github.com>
2024-09-02 08:42:43 +0900

    [Bronze III] Title: 연세여 사랑한다, Time: 80 ms, Memory: 38760 KB -BaekjoonHub

---
+++ 백준/Bronze/25915. 연세여 사랑한다/연세여 사랑한다.py
@@ -0,0 +1,12 @@
+import math
+import sys
+from collections import deque
+import copy
+import datetime
+import math
+from datetime import date, datetime
+
+a = input()
+result = abs(ord('I') - ord(a[0])) + 84
+
+print(result)
