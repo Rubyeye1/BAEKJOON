102a4c1f711b97c3b44bb4e88caaedd5daaf3e06
박형욱 <118232537+Rubyeye1@users.noreply.github.com>
2024-09-01 11:05:21 +0900

    [Bronze III] Title: 거울, 오! 거울, Time: 76 ms, Memory: 38756 KB -BaekjoonHub

---
+++ 백준/Bronze/4740. 거울， 오！ 거울/거울， 오！ 거울.py
@@ -0,0 +1,13 @@
+import math
+import sys
+from collections import deque
+import copy
+import datetime
+import math
+from datetime import date, datetime
+
+while True:
+    temp = input()
+    if temp == "***":
+        break
+    print(temp[::-1])
