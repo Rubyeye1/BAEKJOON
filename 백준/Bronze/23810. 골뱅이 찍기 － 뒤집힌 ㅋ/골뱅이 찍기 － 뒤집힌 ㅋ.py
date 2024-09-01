43f2282deea98fef57f1e06bc1e80e78dcf4af23
박형욱 <118232537+Rubyeye1@users.noreply.github.com>
2024-09-02 08:45:01 +0900

    [Bronze III] Title: 골뱅이 찍기 - 뒤집힌 ㅋ, Time: 152 ms, Memory: 118732 KB -BaekjoonHub

---
+++ 백준/Bronze/23810. 골뱅이 찍기 － 뒤집힌 ㅋ/골뱅이 찍기 － 뒤집힌 ㅋ.py
@@ -0,0 +1,24 @@
+import math
+import sys
+from collections import deque
+import copy
+import datetime
+import math
+from datetime import date, datetime
+
+N = int(sys.stdin.readline())
+
+for i in range(N):
+    print("@@@@@" * N)
+    
+for i in range(N):
+    print("@" * N)
+    
+for i in range(N):
+    print("@@@@@" * N)
+    
+for i in range(N):
+    print("@" * N)
+    
+for i in range(N):
+    print("@" * N)
