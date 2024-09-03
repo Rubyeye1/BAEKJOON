9660df239a25ab7c4867b312b9f3f3a938b96f21
박형욱 <118232537+Rubyeye1@users.noreply.github.com>
2024-09-03 10:42:56 +0900

    [Bronze III] Title: 사격 내기, Time: 96 ms, Memory: 38760 KB -BaekjoonHub

---
+++ 백준/Bronze/27960. 사격 내기/사격 내기.py
@@ -0,0 +1,13 @@
+import math
+import sys
+from collections import deque
+import copy
+import datetime
+import math
+from datetime import date, datetime
+
+sA, sB = map(int, sys.stdin.readline().split())
+
+result = sA ^ sB
+
+print(result)
