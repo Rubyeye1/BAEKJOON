63aefa9f575617707e7440000d58b088b68ab415
박형욱 <118232537+Rubyeye1@users.noreply.github.com>
2024-09-24 10:28:19 +0900

    [Bronze IV] Title: Betting, Time: 104 ms, Memory: 38780 KB -BaekjoonHub

---
+++ 백준/Bronze/24751. Betting/Betting.py
@@ -0,0 +1,10 @@
+import sys
+from collections import deque
+import copy
+import datetime
+import math
+from datetime import date, datetime
+
+a = int(sys.stdin.readline())
+print(100 / a)
+print(100 / (100 - a))
