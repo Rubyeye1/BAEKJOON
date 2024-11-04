b518786caefa3067d8acbb5b0d35032bd7f9dce1
박형욱 <118232537+Rubyeye1@users.noreply.github.com>
2024-11-04 09:33:30 +0900

    [Bronze I] Title: A/B - 2, Time: 108 ms, Memory: 38916 KB, Score: 2000 point -BaekjoonHub

--- 백준/Bronze/15792. A／B － 2/A／B － 2.py
+++ 백준/Bronze/15792. A／B － 2/A／B － 2.py
@@ -1,9 +1,14 @@
-import math
 import sys
 from collections import deque
 import copy
 import datetime
+import math
+from datetime import date, datetime
 
-a, b = map(int,sys.stdin.readline().split())
 
-print(a/b)
+a, b = map(int, sys.stdin.readline().split())
+print(str(a//b) + ".", end="")
+
+for i in range(1000):
+    a = (a % b) * 10
+    print(a//b, end="")
