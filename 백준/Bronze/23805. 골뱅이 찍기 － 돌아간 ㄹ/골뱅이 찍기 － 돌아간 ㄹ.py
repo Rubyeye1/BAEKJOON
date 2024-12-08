49a91272dad292b9a9587b60ca03a67532681b4a
박형욱 <118232537+Rubyeye1@users.noreply.github.com>
2024-12-08 12:21:24 +0900

    [Bronze III] Title: 골뱅이 찍기 - 돌아간 ㄹ, Time: 76 ms, Memory: 38632 KB -BaekjoonHub

---
+++ 백준/Bronze/23805. 골뱅이 찍기 － 돌아간 ㄹ/골뱅이 찍기 － 돌아간 ㄹ.py
@@ -0,0 +1,17 @@
+import sys
+from collections import deque
+import copy
+import datetime
+import math
+from datetime import date, datetime
+
+N = int(sys.stdin.readline())
+
+for i in range(N):
+    print("@@@" * N + " " * N + "@" * N)
+for i in range(N):
+    print("@" * N + " " * N + "@" * N + " " * N + "@" * N)
+    print("@" * N + " " * N + "@" * N + " " * N + "@" * N)
+    print("@" * N + " " * N + "@" * N + " " * N + "@" * N)
+for i in range(N):
+    print("@" * N + " " * N + "@@@" * N)
