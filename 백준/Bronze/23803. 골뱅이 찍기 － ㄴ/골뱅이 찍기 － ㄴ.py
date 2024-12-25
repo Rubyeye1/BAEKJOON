b89a7ada2454ef0e033b54f858c816c0913eb17e
박형욱 <118232537+Rubyeye1@users.noreply.github.com>
2024-12-26 07:43:46 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㄴ, Time: 76 ms, Memory: 38604 KB -BaekjoonHub

---
+++ 백준/Bronze/23803. 골뱅이 찍기 － ㄴ/골뱅이 찍기 － ㄴ.py
@@ -0,0 +1,19 @@
+import sys
+from collections import deque
+import copy
+import datetime
+import math
+from datetime import date, datetime
+
+n = int(sys.stdin.readline())
+
+for i in range(n):
+    print("@" * n)
+for i in range(n):
+    print("@" * n)
+for i in range(n):
+    print("@" * n)
+for i in range(n):
+    print("@" * n)
+for i in range(n):
+    print("@@@@@" * n)
