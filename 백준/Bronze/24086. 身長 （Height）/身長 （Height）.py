ec97577dce8999be082f40bf8dcbdb9036a8b630
박형욱 <118232537+Rubyeye1@users.noreply.github.com>
2025-01-08 10:06:37 +0900

    [Bronze V] Title: 身長 (Height), Time: 76 ms, Memory: 38572 KB -BaekjoonHub

---
+++ 백준/Bronze/24086. 身長 （Height）/身長 （Height）.py
@@ -0,0 +1,11 @@
+import sys
+from collections import deque
+import copy
+import datetime
+import math
+from datetime import date, datetime
+
+A = int(sys.stdin.readline())
+B = int(sys.stdin.readline())
+
+print(abs(A-B))
