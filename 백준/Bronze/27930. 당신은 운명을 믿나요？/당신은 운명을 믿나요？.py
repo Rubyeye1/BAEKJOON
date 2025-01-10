6628f79e6d88b35fcb872a705777f2bea0be2fb7
박형욱 <118232537+Rubyeye1@users.noreply.github.com>
2025-01-10 10:21:19 +0900

    [Bronze I] Title: 당신은 운명을 믿나요?, Time: 96 ms, Memory: 42868 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/27930. 당신은 운명을 믿나요？/당신은 운명을 믿나요？.py
+++ 백준/Bronze/27930. 당신은 운명을 믿나요？/당신은 운명을 믿나요？.py
@@ -8,48 +8,28 @@ from datetime import date, datetime
 L = input()
 L = list(L)
 
-K = []
-Y = []
+K = ["K","O","R","E","A"]
+Y = ["Y","O","N","S","E","I"]
+
+Ktemp = 0
+Ytemp = 0
 
 for i in range(len(L)):
 
-    if L[i] == "K" and "K" not in K:
-        K.append(L[i])
-    if L[i] == "Y" and "Y" not in Y:
-        Y.append(L[i])
+    if L[i] == K[Ktemp] and L[i] == Y[Ytemp]:
+        Ktemp += 1
+        Ytemp += 1
+    elif L[i] == K[Ktemp]:
+        Ktemp += 1
+    elif L[i] == Y[Ytemp]:
+        Ytemp += 1
 
-    if L[i] == "O":
-        if "K" in K and "O" not in K:
-            K.append(L[i])
-        elif "Y" in Y and "O" not in Y:
-            Y.append(L[i])
-
-    if L[i] == "N" and "O" in Y and "N" not in Y:
-        Y.append(L[i])
-
-    if L[i] == "R" and "O" in K and "R" not in K:
-        K.append(L[i])
-
-    if L[i] == "S" and "N" in Y and "S" not in Y:
-        Y.append(L[i])
-
-    if L[i] == "E":
-        if "R" in K and "E" not in K:
-            K.append(L[i])
-        elif "S" in Y and "E" not in Y:
-            Y.append(L[i])
-
-    if L[i] == "I" and "E" in Y and "I" not in Y:
-        Y.append(L[i])
-
-    if L[i] == "A" and "E" in K and "A" not in K:
-        K.append(L[i])
-
-    if len(Y) == 6:
-        for j in range(len(Y)):
-            print(Y[j], end="")
-        break
-    elif len(K) == 5:
+    if Ktemp == 5:
         for j in range(len(K)):
             print(K[j], end="")
+        break
+
+    if Ytemp == 6:
+        for j in range(len(Y)):
+            print(Y[j], end="")
         break
