2b291407aee8248ebd05a2d015fc7058c21cd2b6
박형욱 <118232537+Rubyeye1@users.noreply.github.com>
2025-04-22 06:38:57 +0900

    [Bronze IV] Title: 입실 관리, Time: 124 ms, Memory: 41556 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5524. 입실 관리/입실 관리.py
@@ -0,0 +1,13 @@
+import sys
+from collections import deque
+import copy
+import datetime
+import math
+from datetime import date, datetime
+import re
+
+N = int(sys.stdin.readline())
+
+for _ in range(N):
+    temp = input()
+    print(temp.lower())
