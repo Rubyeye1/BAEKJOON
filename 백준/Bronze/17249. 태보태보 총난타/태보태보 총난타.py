a751db027c07dd4e36475b065f5e87b885ea0bae
박형욱 <118232537+Rubyeye1@users.noreply.github.com>
2025-04-25 06:50:41 +0900

    [Bronze II] Title: 태보태보 총난타, Time: 124 ms, Memory: 41428 KB -BaekjoonHub

---
+++ 백준/Bronze/17249. 태보태보 총난타/태보태보 총난타.py
@@ -0,0 +1,11 @@
+import sys
+from collections import deque
+import copy
+import datetime
+import math
+from datetime import date, datetime
+import re
+
+y, o = input().split("(^0^)")
+
+print(y.count("@"), o.count("@"))
