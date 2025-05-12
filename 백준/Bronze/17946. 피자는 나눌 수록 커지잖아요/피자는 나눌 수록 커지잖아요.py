bf96f456fb8ad220f6ecb4d5755aab25277332c1
박형욱 <118232537+Rubyeye1@users.noreply.github.com>
2025-05-12 09:24:52 +0900

    [Bronze II] Title: 피자는 나눌 수록 커지잖아요, Time: 124 ms, Memory: 41428 KB -BaekjoonHub

---
+++ 백준/Bronze/17946. 피자는 나눌 수록 커지잖아요/피자는 나눌 수록 커지잖아요.py
@@ -0,0 +1,18 @@
+import sys
+from collections import deque
+import copy
+import datetime
+import math
+from datetime import date, datetime
+import re
+
+N = int(sys.stdin.readline())
+
+while True:
+    temp = int(sys.stdin.readline())
+    
+    N -= 1
+    print(1)
+    
+    if N == 0:
+        break
