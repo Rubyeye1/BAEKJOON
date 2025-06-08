ab949f424110d7ed32cfcb324ba95af18520e8ec
Rubyeye <118232537+Rubyeye1@users.noreply.github.com>
2025-06-08 09:59:43 +0900

    [Bronze III] Title: 택시 기하학, Time: 136 ms, Memory: 41428 KB -BaekjoonHub

---
+++ 백준/Bronze/3053. 택시 기하학/택시 기하학.py
@@ -0,0 +1,12 @@
+import sys
+from collections import deque
+import copy
+import datetime
+import math
+from datetime import date, datetime
+import re
+
+R = int(sys.stdin.readline())
+
+print(R * R * math.pi)
+print(R * R * 2)
