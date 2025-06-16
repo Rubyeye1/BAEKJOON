e4b7218fa261f52c0f1cec1b5c3c464b9d629101
Rubyeye <118232537+Rubyeye1@users.noreply.github.com>
2025-06-16 09:07:50 +0900

    [Silver V] Title: 문서 검색, Time: 140 ms, Memory: 41428 KB -BaekjoonHub

---
+++ 백준/Silver/1543. 문서 검색/문서 검색.py
@@ -0,0 +1,12 @@
+import sys
+from collections import deque
+import copy
+import datetime
+import math
+from datetime import date, datetime
+import re
+
+ms = input()
+gs = input()
+result = ms.split(gs)
+print(len(result) - 1)
