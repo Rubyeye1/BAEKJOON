aeace6d62cd0c3fbefcbf9f4721b848301295f2d
Rubyeye <118232537+Rubyeye1@users.noreply.github.com>
2025-06-17 07:06:29 +0900

    [Bronze V] Title: Rook, Time: 120 ms, Memory: 41444 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.py
@@ -0,0 +1,21 @@
+import sys
+from collections import deque
+import copy
+import datetime
+import math
+from datetime import date, datetime
+import re
+
+print('  ___  ___  ___')
+print('  | |__| |__| |')
+print('  |           |')
+print('   \_________/')
+print('    \_______/')
+print('     |     |')
+print('     |     |')
+print('     |     |')
+print('     |     |')
+print('     |_____|')
+print('  __/       \__')
+print(' /             \\')
+print('/_______________\\')
