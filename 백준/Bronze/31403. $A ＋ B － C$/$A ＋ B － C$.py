91a0b8199d2779a3bc1ed0ab5ef9692e08ac2046
Rubyeye <118232537+Rubyeye1@users.noreply.github.com>
2025-06-26 10:57:43 +0900

    [Bronze IV] Title: $A + B - C$, Time: 128 ms, Memory: 41440 KB -BaekjoonHub

---
+++ 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.py
@@ -0,0 +1,14 @@
+import sys
+from collections import deque
+import copy
+import datetime
+import math
+from datetime import date, datetime
+import re
+
+A = input()
+B = input()
+C = input()
+
+print(int(A) + int(B) - int(C))
+print(int(A + B) - int(C))
