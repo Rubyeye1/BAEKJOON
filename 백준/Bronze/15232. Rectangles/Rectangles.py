eeab429607cd1a793f19b29ee9240f777cf5ebc2
Rubyeye <118232537+Rubyeye1@users.noreply.github.com>
2025-07-11 11:22:36 +0900

    [Bronze V] Title: Rectangles, Time: 124 ms, Memory: 41448 KB -BaekjoonHub

---
+++ 백준/Bronze/15232. Rectangles/Rectangles.py
@@ -0,0 +1,13 @@
+import sys
+from collections import deque
+import copy
+import datetime
+import math
+from datetime import date, datetime
+import re
+
+R = int(sys.stdin.readline())
+C = int(sys.stdin.readline())
+
+for i in range(R):
+    print('*' * C)
