98c0cb58ce8889d113259820b0ca5619c5842352
Rubyeye <118232537+Rubyeye1@users.noreply.github.com>
2025-09-10 10:13:58 +0900

    [Bronze II] Title: 스물셋, Time: 332 ms, Memory: 41448 KB -BaekjoonHub

---
+++ 백준/Bronze/23251. 스물셋/스물셋.py
@@ -0,0 +1,13 @@
+import sys
+from collections import deque
+import copy
+import datetime
+import math
+from datetime import date, datetime
+import re
+
+T = int(sys.stdin.readline())
+
+for i in range(T):
+    temp = int(sys.stdin.readline())
+    print(temp * 23)
