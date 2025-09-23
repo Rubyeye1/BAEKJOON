93c79847d1e5b72020945e51f90d3b56640a07ba
Rubyeye <118232537+Rubyeye1@users.noreply.github.com>
2025-09-23 14:03:23 +0900

    [Bronze I] Title: 바닥수, Time: 132 ms, Memory: 41472 KB -BaekjoonHub

---
+++ 백준/Bronze/30456. 바닥수/바닥수.py
@@ -0,0 +1,14 @@
+import sys
+from collections import deque
+import copy
+import datetime
+import math
+from datetime import date, datetime
+import re
+
+N, L = map(int, sys.stdin.readline().split())
+
+for _ in range(L - 1):
+    print(1, end="")
+
+print(N)
