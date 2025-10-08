8f63174103329e80b64ad9fbdf7a2043e541cc96
Rubyeye <118232537+Rubyeye1@users.noreply.github.com>
2025-10-08 11:08:32 +0900

    [Bronze V] Title: AI 선도대학, Time: 132 ms, Memory: 40668 KB -BaekjoonHub

---
+++ 백준/Bronze/32951. AI 선도대학/AI 선도대학.py
@@ -0,0 +1,11 @@
+import sys
+from collections import deque
+import copy
+import datetime
+from datetime import date, datetime
+import re
+import math
+
+N = int(sys.stdin.readline())
+
+print(N - 2024)
