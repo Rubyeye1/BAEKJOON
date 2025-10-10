0a54393531e8a8964cee5000f6cf736e7da7f59d
Rubyeye <118232537+Rubyeye1@users.noreply.github.com>
2025-10-11 07:35:03 +0900

    [Bronze IV] Title: Abbey Courtyard, Time: 124 ms, Memory: 40668 KB -BaekjoonHub

---
+++ 백준/Bronze/15610. Abbey Courtyard/Abbey Courtyard.py
@@ -0,0 +1,11 @@
+import sys
+from collections import deque
+import copy
+import datetime
+from datetime import date, datetime
+import re
+import math
+
+a = int(sys.stdin.readline())
+
+print(a ** 0.5 * 4)
